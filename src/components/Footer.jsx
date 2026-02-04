2f1cf0209d6cdaad0f403d67eeda8a7382dd5962
SirioRedGiant <Oiris1992@gmail.com>
2026-02-04 18:41:12 +0100

    ggg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import digitalComicsIcon from "../assets/img/buy-comics-digital-comics.png"
-
+import merchandiseIcon from "../assets/img/buy-comics-merchandise.png"
+import subscriptionIcon from "../assets/img/buy-comics-subscriptions.png"
+import comicShopLocator from "../assets/img/buy-comics-shop-locator.png"
+import dcPowerVisa from "../assets/img/buy-dc-power-visa.svg"
 
 
 export default function Footer() {
@@ -7,9 +10,20 @@ export default function Footer() {
     <div className="bg-primary py-5">
       <div className="container d-flex justify-content-around align-items-center">
         <div className="d-flex align-items-center text-white">
-          <img src={digitalComicsIcon} alt="" />
+          <img src={digitalComicsIcon} alt="DIGITAL COMICS ICON" />
         </div>
-
+        <div className="d-flex align-items-center text-white">
+          <img src={merchandiseIcon} alt="DIGITAL COMICS ICON" />
+        </div>
+        <div className="d-flex align-items-center text-white">
+          <img src={subscriptionIcon} alt="DIGITAL COMICS ICON" />
+        </div>
+        <div className="d-flex align-items-center text-white">
+          <img src={comicShopLocator} alt="DIGITAL COMICS ICON" />
+        </div>
+        <div className="d-flex align-items-center text-white">
+          <img src={dcPowerVisa} alt="DIGITAL COMICS ICON" />
+        </div>
       </div>
     </div>
   </footer>;
