5b8a6af128150adeb815f472fd1d0f9521e7dc7d
SirioRedGiant <Oiris1992@gmail.com>
2026-02-04 17:52:56 +0100

    importata immagine del logo nell'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import dcLogo from "../assets/img/dc-logo.png"
+
 export default function Header() {
   return <header className="bg-white">
     <div className="container d-flex jusify-content-between align-items-center">
       <div className="logo-container">
-      <img src="src\assets\img\dc-logo.png" alt="DC logo" />
+      <img src={dcLogo} alt="DC logo" />
       </div>
-      
+
     </div>
   </header>;
 }
