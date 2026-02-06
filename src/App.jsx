d033526b8b4f40c1f8506cac0dbb1f1a5f5950d3
SirioRedGiant <Oiris1992@gmail.com>
2026-02-06 17:49:06 +0100

    fixed images paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import headLogoImg from "./assets/img/dc-logo.png";
+import jumbotronImg from "./assets/img/jumbotron.jpg";
 
 //note NAVLINKS --> Header links
 import navLinks from "./data/links";
@@ -14,8 +16,8 @@ import Jumbotron from "./components/Jumbotron";
 export default function App() {
   return (
     <>
-      <Header headLogoImg="src/assets/img/dc-logo.png" links={navLinks} />
-      <Jumbotron image="/img/jumbotron.jpg" />
+      <Header headLogoImg={headLogoImg} links={navLinks} />
+      <Jumbotron image={jumbotronImg} />
       <Main comics={comicsListData} />
       <Footer />
     </>
